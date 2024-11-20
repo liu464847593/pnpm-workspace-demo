a0b1150ce58beaf1c3f4bdcaf61fedd240e15c6b
liuzhijian <18576746147@163.com>
2024-11-20 16:47:38 +0800

    feat: 新增button组件测试打包生成button.js

---
+++ packages/components/button/index.js
@@ -0,0 +1,5 @@
+import CsButton from './src/main.vue';
+CsButton.install = function(Vue) {
+  Vue.component(CsButton.name, CsButton);
+}
+export default CsButton
---
+++ packages/components/button/src/main.vue
@@ -0,0 +1,17 @@
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "CsButton"
+})
+</script>
+
+<template>
+<el-button type="primary" class="button">test</el-button>
+</template>
+
+<style scoped>
+.button{
+  width: 300px;
+}
+</style>
