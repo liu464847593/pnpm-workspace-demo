74be5e31d757ce973f8619001d48b724e6236eaf
liuzhijian <18576746147@163.com>
2024-11-20 19:43:47 +0800

    feat: elementPlus按需加载

--- play/main.js
+++ play/main.js
@@ -1,12 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-
-
 import DemoComponents from '@l-org/pnpm-workspace-demo-components'
 const app = createApp(App)
-app.use(ElementPlus)
 app.use(DemoComponents)
 app.mount('#app')
